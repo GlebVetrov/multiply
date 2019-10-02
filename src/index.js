c9165ac9418811daa441b28ac40c4038e5e19915
BigGlebovsky <3696732@mail.ru>
2019-10-02 22:23:06 +0300

    feat: add task with solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function multiply(first, second) {
+function multiply(first, second) {
   // your solution
+        const num1 = first;
+        const num2 = second;
+
+        let stack = [];
+
+        for (let i = 0; i < num1.length; i++) {
+            for (let j = 0; j < num2.length; j++) {
+                let mul = num1[i] * num2[j];
+                stack[i + j] = stack[i + j] ? +stack[i + j] + mul : mul;
+            }
+        }
+
+        let strArray = [];
+
+        stack.reverse().forEach((elem, i) => {
+            let num = elem % 10;
+            let remainder = Math.floor(elem / 10);
+            if (strArray[i]) {
+                elem = num + strArray[i];
+                num = elem % 10;
+                remainder = remainder + Math.floor(elem / 10);
+            }
+            strArray[i] = num;
+            if (remainder !== 0) {
+                strArray[i + 1] = remainder;
+            }
+        });
+    return strArray.reverse().join('');
+    
 }
+
+module.exports = multiply;
